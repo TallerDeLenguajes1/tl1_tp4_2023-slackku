e11d5e4fefdc75b4e0a4ef019a9f6f047b1b20fa
slacku <bananuya3@gmail.com>
2023-04-20 11:51:45 -0300

    Punto 2.(1,2,3,4,5) Terminados

--- tl1_tp4_p2.c
+++ tl1_tp4_p2.c
@@ -10,14 +10,14 @@ typedef struct Tarea
     int Duracion; // entre 10 – 100
 } Tarea;
 
+void initializeLists(int cantidad, Tarea **pendientes, Tarea **terminadas);
 void chargeToDoList(int cantidad, Tarea **pendientes);
 void reqDuration(int *duracion);
-void showAskDone(int cantidad, Tarea **pendientes, Tarea **terminadas);
+void askIfDone(int cantidad, Tarea **pendientes, Tarea **terminadas);
 int isTareaFinished(int idTarea);
 int isRightChar(char *character);
-void askIfCompleted(char *character, int idTarea);
 void moveFinishedTarea(int cantidad, Tarea *pendientes, Tarea **terminadas);
-void retireFromPendientes(int cantidad, int id, Tarea **pendientes);
+void showLists(int cantidad, Tarea **pendientes, Tarea **terminadas);
 int main()
 {
     srand(time(NULL));
@@ -25,31 +25,30 @@ int main()
 
     printf("Ingrese la cantidad de tareas a cargar: ");
     scanf("%d", &cTareas);
-
+    fflush(stdin);
     Tarea **listaTareasPendientes = (Tarea **)malloc(sizeof(Tarea *) * cTareas);
     Tarea **listaTareasTerminadas = (Tarea **)malloc(sizeof(Tarea *) * cTareas);
 
-    for (int i = 0; i < cTareas; i++)
-    {
-        listaTareasPendientes[i] = (Tarea *)malloc(sizeof(Tarea));
-        listaTareasTerminadas[i] = (Tarea *)malloc(sizeof(Tarea));
-    }
-
-    // for (int i = 0; i < cTareas; i++)
-    // {
-    //     listaTareasPendientes[i] = NULL;
-    //     listaTareasTerminadas[i] = NULL;
-    // }
-
+    initializeLists(cTareas, listaTareasPendientes, listaTareasTerminadas);
     chargeToDoList(cTareas, listaTareasPendientes);
-
-    showAskDone(cTareas, listaTareasPendientes, listaTareasTerminadas);
+    askIfDone(cTareas, listaTareasPendientes, listaTareasTerminadas);
+    showLists(cTareas, listaTareasPendientes, listaTareasTerminadas);
 
     return 0;
 }
 
-void chargeToDoList(int cantidad, Tarea **pendientes)
+void initializeLists(int cantidad, Tarea **pendientes, Tarea **terminadas) // Checked
 {
+    for (int i = 0; i < cantidad; i++) // Inicializa en cada puntero tarea, las tareas, en NULL
+    {
+        pendientes[i] = NULL;
+        terminadas[i] = NULL;
+    }
+}
+
+void chargeToDoList(int cantidad, Tarea **pendientes) // Checked
+{
+    fflush(stdin);
     printf("|===========================|\n");
     printf("|---    Cargar Tareas    ---|\n");
     printf("|===========================|\n");
@@ -57,25 +56,33 @@ void chargeToDoList(int cantidad, Tarea **pendientes)
     for (int i = 0; i < cantidad; i++)
     {
         printf("Tarea N: %d\n", i + 1);
-        int *duracion;
+
+        int *duracion = (int *)malloc(sizeof(int));
         char *buffer = malloc(sizeof(char) * 50);
+
         printf("Ingrese la descripcion: ");
         gets(buffer);
         fflush(stdin);
 
+        pendientes[i] = (Tarea *)malloc(sizeof(Tarea)); // A cada fila del arreglo de arreglo pendientes, le asigno memoria dinamica
         pendientes[i]->TareaID = i + 1;
+
         pendientes[i]->Descripcion = malloc((strlen(buffer) + 1) * sizeof(char));
         strcpy(pendientes[i]->Descripcion, buffer);
-
+        free(buffer);
         reqDuration(duracion);
+
         pendientes[i]->Duracion = *duracion;
+        free(duracion);
+        printf("---------------------------\n");
     }
 }
 
-void reqDuration(int *duracion)
+void reqDuration(int *duracion) // Checked
 {
-    printf("Ingrese la cantidad de clientes: ");
-    scanf("%d", duracion);
+    printf("Ingrese la duracion: "); // Obtencion y control de datos ingresados de duracion
+    scanf("%d", duracion);           // Para que cumpla con pertenecer al intervalo [10,100]
+    fflush(stdin);
     if (*duracion < 10)
     {
         printf("Error. Duracion menor a la minima. (Min: 10)\n");
@@ -88,74 +95,95 @@ void reqDuration(int *duracion)
     }
 }
 
-void showAskDone(int cantidad, Tarea **pendientes, Tarea **terminadas)
+void askIfDone(int cantidad, Tarea **pendientes, Tarea **terminadas) // Checked
 {
     for (int i = 0; i < cantidad; i++)
     {
         printf("|===========================|\n");
-        printf("|---     TAREA N: %d     ---|\n", pendientes[i]->TareaID);
+        printf("|---     TAREA N: %2d     ---|\n", pendientes[i]->TareaID);
         printf("|===========================|\n");
         printf("Descripcion: ");
         puts(pendientes[i]->Descripcion);
         printf("Duracion: %d\n", pendientes[i]->Duracion);
-
-        if (isTareaFinished(pendientes[i]->TareaID))
+        int isFinishedResult;
+        isFinishedResult = isTareaFinished(pendientes[i]->TareaID);
+        if (isFinishedResult)
         {
             moveFinishedTarea(cantidad, pendientes[i], terminadas);
-            retireFromPendientes(cantidad, pendientes[i]->TareaID, pendientes);
+            pendientes[i] = NULL;
+            // Una vez movida la tarea pendiente a terminada, pendiente queda en null
         }
     }
 }
 
-int isTareaFinished(int idTarea)
+int isTareaFinished(int idTarea) // Checked
 {
     int result;
-    char *buff;
-    askIfCompleted(buff, idTarea);
+    char *buff = (char *)malloc(sizeof(char));
+    printf("La tarea N: %d, fue completada? : (y/n)\n", idTarea);
+    *buff = getchar();
+    fflush(stdin);
     if (!isRightChar(buff))
     {
-        askIfCompleted(buff, idTarea);
+        printf("El caracter ingresado es incorrecto\n");
+        isTareaFinished(idTarea);
     }
     if (*buff == 'y')
     {
-        return 1;
+        result = 1;
     }
+
     if (*buff == 'n')
     {
-        return 0;
+        result = 0;
     }
+    free(buff);
+    return result;
 }
 
-int isRightChar(char *character)
+int isRightChar(char *character) // Checked
 {
     return ((*character == 'y') || (*character == 'n'));
 }
 
-void askIfCompleted(char *character, int idTarea)
-{
-    printf("La tarea N: %d, fue completada? : (y/n)\n");
-    *character = getchar();
-}
-
-void moveFinishedTarea(int cantidad, Tarea *pendientes, Tarea **terminadas)
+void moveFinishedTarea(int cantidad, Tarea *pendientes, Tarea **terminadas) // Checked
 {
     for (int i = 0; i < cantidad; i++)
     {
-        if (terminadas[i] != NULL)
+        if (terminadas[i] == NULL) // Si ese espacio de Terminadas es nulo, lo sobreescribo
         {
+            terminadas[i] = (Tarea *)malloc(sizeof(Tarea));
             terminadas[i] = pendientes;
             i = cantidad;
         }
     }
 }
 
-void retireFromPendientes(int cantidad, int id, Tarea **pendientes)
+void showLists(int cantidad, Tarea **pendientes, Tarea **terminadas) // Checked
 {
     for (int i = 0; i < cantidad; i++)
     {
-        if (pendientes[i]->TareaID == id)
+        if (pendientes[i] != NULL)
         {
-            pendientes[i] = NULL;
+            printf("|------LISTA PENDIENTES-----|\n");
+            printf("|===========================|\n");
+            printf("|---     TAREA N: %2d     ---|\n", pendientes[i]->TareaID);
+            printf("|===========================|\n");
+            printf("Descripcion: %s\n", pendientes[i]->Descripcion);
+            printf("Duracion: %d\n", pendientes[i]->Duracion);
+        }
+    }
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (terminadas[i] != NULL)
+        {
+            printf("|------LISTA TERMINADAS-----|\n");
+            printf("|===========================|\n");
+            printf("|---     TAREA N: %2d     ---|\n", terminadas[i]->TareaID);
+            printf("|===========================|\n");
+            printf("Descripcion: %s\n", terminadas[i]->Descripcion);
+            printf("Duracion: %d\n", terminadas[i]->Duracion);
+            printf("\n");
         }
     }
 }
